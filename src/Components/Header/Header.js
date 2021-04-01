b93573a81d77755fc092d560df3f9aba67e2b261
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-04-02 02:29:44 +0600

    Assignment-completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,8 @@ import React, { useContext } from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../App';
-import './Header.css'
+import './Header.css';
+import bookLogo from '../../icons/book-logo.png'
 
 const Header = () => {
     const [loggedInUser,setLoggedInUser] = useContext(UserContext);
@@ -10,7 +11,7 @@ const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="white" variant="white">
             <div className="container">
-                <Link className="navbar-brand nav-brand" to="/">Book-shop</Link>
+                <Link className="navbar-brand nav-brand" to="/"> <img className="img-fluid book-logo" src={bookLogo} alt=""/> Pick-a-Book</Link>
                 <Navbar.Toggle className="nav-toggle"><img src='https://img.icons8.com/ios/30/000000/menu--v6.png' alt="" /></Navbar.Toggle>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">
