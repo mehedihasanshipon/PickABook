c20f6a6ef0dfd230d3211750901cd8855c09f066
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-04-02 01:49:44 +0600

    Assignment completed

--- src/Components/AddBooks/AddBooks.js
+++ src/Components/AddBooks/AddBooks.js
@@ -3,33 +3,31 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import "./AddBooks.css";
 
-
 const AddBooks = () => {
   const { register, handleSubmit } = useForm();
-  const [imgUrl,setImgUrl] = useState({});
-  console.log({imgUrl});
-  const onSubmit = data => {
+  const [imgUrl, setImgUrl] = useState({});
+  console.log({ imgUrl });
+  const onSubmit = (data) => {
     // console.log(data)
     const books = {
-        name: data.name,
-        price: data.price,
-        author: data.author,
-        bookImage: imgUrl
-    }
+      name: data.name,
+      price: data.price,
+      author: data.author,
+      bookImage: imgUrl,
+    };
 
-    books.bookImage && axios
+    axios
       .post("http://localhost:3002/addBooks", books)
       .then(function (response) {
-        console.log('Server side result',response);
+        console.log("Server side result", response);
       })
       .catch(function (error) {
         console.log(error);
       });
-      console.log(books);
-
+    console.log(books);
   };
 
-  const handleImageUpload = (e)=>{
+  const handleImageUpload = (e) => {
     // console.log(e.target.files[0]);
     const imgData = new FormData();
     imgData.set("key", "baf759f7b47b24e2b89feea41140dc74");
@@ -45,31 +43,67 @@ const AddBooks = () => {
       .catch(function (error) {
         console.log(error);
       });
-  }
+  };
   return (
     <div>
       <div className="input-section d-flex justify-content-center">
-      
         <div className="book-info">
-        
           <div className="form-section p-4">
-          <h4 className="text-center">Add Book</h4>
+            <h4 className="text-center">Add Book</h4>
             <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="name" className="form-label">Name</label>
-              <input name="name" className="form-control" placeholder="Enter Name" ref={register} />
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <input
+                name="name"
+                className="form-control"
+                placeholder="Enter Name"
+                ref={register}
+              />
 
-              <label htmlFor="price" className="form-label">Add price</label>
-              <input name="price" className="form-control" placeholder="Enter Price" ref={register} />
+              <label htmlFor="price" className="form-label">
+                Add price
+              </label>
+              <input
+                name="price"
+                className="form-control"
+                placeholder="Enter Price"
+                ref={register}
+              />
 
-              <label htmlFor="author" className="form-label">Author Name</label>
-              <input name="author" className="form-control" placeholder="Enter Author Name" ref={register} />
-              
-                <label for="formFile" className="form-label">Add book cover photo</label>
-                <input onChange={handleImageUpload} className="form-control" type="file" id="formFile" />
-              
-              <input type="submit" value="Save"  className="form-control btn btn-primary my-2" />
+              <label htmlFor="author" className="form-label">
+                Author Name
+              </label>
+              <input
+                name="author"
+                className="form-control"
+                placeholder="Enter Author Name"
+                ref={register}
+              />
 
-              
+              <label for="formFile" className="form-label">
+                Add book cover photo
+              </label>
+              <input
+                onChange={handleImageUpload}
+                className="form-control"
+                type="file"
+                id="formFile"
+              />
+              {imgUrl.length > 0 ? (
+                <input
+                  type="submit"
+                  value="Add Book"
+                  className="form-control btn btn-primary my-2"
+                />
+              ) : (
+                <input
+                  type="submit"
+                  value="Add Book"
+                  disabled
+                  className="form-control btn btn-primary my-2"
+                />
+              )}
             </form>
           </div>
         </div>
--- src/Components/ManageBookList/ManageBookList.js
+++ src/Components/ManageBookList/ManageBookList.js
@@ -1,8 +1,8 @@
 import React from "react";
-import editIcon from '../../icons/edit 1.png';
-import deleteIcon from '../../icons/delete.png'
+import { useHistory } from "react-router";
 
 const ManageBookList = (props) => {
+  const history = useHistory()
     // console.log(props.book);
     const {author,name,price,_id} = props.book;
     const handleDeleteClick= (id)=>{
@@ -14,6 +14,7 @@ const ManageBookList = (props) => {
         .then(result=>{
             if(result){
                 console.log("Deleted");
+                // history.go(0);
             }
         })
     }
@@ -23,7 +24,7 @@ const ManageBookList = (props) => {
         <td> {name} </td>
         <td>{author}</td>
         <td>${price}</td>
-        <td><button className="btn"><img src="https://img.icons8.com/bubbles/50/000000/edit.png" alt=""/></button> 
+        <td><button className="btn"><img src="https://img.icons8.com/bubbles/40/000000/edit.png" alt=""/></button> 
         <button onClick={()=>handleDeleteClick(_id)} className="btn "><img src="https://img.icons8.com/cute-clipart/40/000000/delete-forever.png" alt=""/></button></td>
       </tr>
     </tbody>
